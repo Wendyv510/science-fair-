27f96cca110efe22cb65d8f200aa904b906618d2
Wendy Harris <wendyv510@hotmail.com>
2020-02-07 19:22:49 +0000

    AppController RESTful routes, student files, edit/delete forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,12 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
   
-  get '/user/signup' do 
+  get '/signup' do 
     
     erb :'/user/signup'
   end 
   
-  post '/user/signup' do 
+  post '/signup' do 
     user = User.new(:name => params[:name], :email => params[:email], :password => params[:password]) 
     
     if user.save
@@ -28,12 +28,12 @@ class ApplicationController < Sinatra::Base
     end 
   end 
   
-  get '/user/login' do 
+  get '/login' do 
     
     erb :'/user/login' 
   end 
   
-  post "/user/login" do 
+  post "/login" do 
     user = User.find_by(:email => params[:email]) 
     
       if user && user.authenticate(params[:password])
@@ -42,6 +42,54 @@ class ApplicationController < Sinatra::Base
       else 
           redirect to '/user/failure' 
       end 
+  end 
+  
+  get '/logout' do 
+    if Helpers.is_logged_in?(session)
+      session.clear 
+    else 
+      redirect to '/' 
+    end 
+  end 
+  
+  get '/students' do 
+    @students = Student.all 
+      erb :index 
+  end 
+  
+  get '/students/new' do 
+    erb :'/students/new' 
+  end 
+  
+  post '/students' do 
+    @student = Student.create(:name => params[:name], :grade_level params[:grade_level], :project => params[:project])
+    @student.save 
+    redirect to "/students/#{@student.id}"
+  end 
+  
+  get '/students/:id' do 
+    @student = Student.find_by_id(params[:id]) 
+       erb :show 
+  end 
+  
+  get '/students/:id/edit' do 
+    @student = Student.find_by_id(params[:id]) 
+       erb :edit 
+  end 
+  
+  patch '/students/:id' do 
+    @student = Student.find_by_id(params[:id]) 
+    @student.name = params[:name] 
+    @student.grade_level = params[:grade_level] 
+    @student.project = params[:project] 
+    @student.save 
+       redirect to "/students/#{@student.id}" 
+  end 
+  
+  delete '/students/:id' do 
+    @student = Student.find_by_id(params[:id]) 
+    @student.delete 
+       redirect to '/students' 
   end 
       
 
